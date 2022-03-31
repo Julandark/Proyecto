cf5b7082bc35ed9d93889bdefd0db736c90b95f1
Julandark <102311651+Julandark@users.noreply.github.com>
2022-03-31 14:21:26 -0500

    Add files via upload

---
+++ proyectodb/404.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Error404</title>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+    <style>
+        *{
+    margin: 0;
+    padding: 0;
+    box-sizing: borde-box;
+    font-family: 'PT Sans', sans-serif;
+        }
+        body{
+            background: #827870;
+            width: 100%;
+            height: 100vh;
+        }
+ 
+    }   
+    </style>
+</head>
+<body>
+    <div class="container-404">
+    <p>Esta pagina no se encuentra disponible en este momento (╥︣﹏᷅╥)</p>
+    <a href="index.php" class="boton">
+    Página Principal
+    </a>
+
+    </div>
+</body>
+</html>
